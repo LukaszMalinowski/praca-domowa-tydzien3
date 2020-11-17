912f621cad2cb1a5ce0f89ce40729ef65b4f0ee0
Lukasz Malinowski <lukasz.malinowski@uniteam.pl>
2020-11-17 18:55:04 +0100

    initial commit

---
+++ src/main/java/pl/lukaszmalina/tydzien3/Tydzien3Application.java
@@ -0,0 +1,13 @@
+package pl.lukaszmalina.tydzien3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tydzien3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tydzien3Application.class, args);
+    }
+
+}
