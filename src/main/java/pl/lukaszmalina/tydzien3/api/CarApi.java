78fc611b08f1eb4ef0cb6c608f9477ccedadb42a
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2020-11-17 20:01:02 +0100

    add repository implementation

---
+++ src/main/java/pl/lukaszmalina/tydzien3/api/CarApi.java
@@ -0,0 +1,10 @@
+package pl.lukaszmalina.tydzien3.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
+public class CarApi {
+
+}
---
+++ src/main/java/pl/lukaszmalina/tydzien3/repository/CarRepository.java
@@ -0,0 +1,59 @@
+package pl.lukaszmalina.tydzien3.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.lukaszmalina.tydzien3.entity.Car;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarRepository{
+
+    private List<Car> carList;
+
+    public CarRepository() {
+        this.carList = new ArrayList<>();
+    }
+
+    public List<Car> getAllCars() {
+        return carList;
+    }
+
+    public Optional<Car> getCarById(long id) {
+        return carList.stream().filter(car -> car.getId() == id).findFirst();
+    }
+
+    public List<Car> getCarsByColor(Color color) {
+        return carList.stream().filter(car -> car.getColor().equals(color)).collect(Collectors.toList());
+    }
+
+    public void addCar(Car car) {
+        carList.add(car);
+    }
+
+    public boolean editCar(Car newCar) {
+        Optional<Car> optionalCar = carList.stream().filter(oldCar -> oldCar.getId() == newCar.getId()).findFirst();
+
+        if (optionalCar.isPresent()) {
+            carList.remove(optionalCar.get());
+            carList.add(newCar);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean removeCarById(long id) {
+        Optional<Car> optionalCar = carList.stream().filter(oldCar -> oldCar.getId() == id).findFirst();
+
+        if(optionalCar.isPresent()) {
+            carList.remove(optionalCar.get());
+            return true;
+        }
+
+        return false;
+    }
+}
